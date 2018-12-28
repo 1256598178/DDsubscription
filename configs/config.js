3450d6200fab1c9ddcd03eae1655efc0ca3a8e38
MS-20170423CWSV\Administrator <1029688400@qq.com>
2018-12-29 00:12:16 +0800

    修改 import 和require的冲突

--- configs/config.js
+++ configs/config.js
@@ -17,7 +17,7 @@ const config = {
     // Various Dev Server settings
     contentBase: ROOT,
     host: ip,
-    port: 8081,
+    port: 8088,
     historyApiFallback: true,
     open: true,
     watchContentBase: true,
